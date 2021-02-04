2b3f6f7d99a848ff7d3c9a9959f4aecfdc2400c9
Darren Kopa <dkopa@andrew.cmu.edu>
2021-02-03 19:54:45 -0500

    added child name function and alphabetical, active scopes

--- app/models/child.rb
+++ app/models/child.rb
@@ -4,4 +4,11 @@ class Child < ApplicationRecord
 
   validates_presence_of :first_name
   validates_presence_of :last_name
+
+  def name
+    return "#{self.first_name} #{self.last_name}"
+  end
+
+  scope :alphabetical, -> { order(:last_name).order(:first_name) }
+  scope :active, -> { where(active: true) }
 end
